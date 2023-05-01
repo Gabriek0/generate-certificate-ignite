a48e5cf64a2d2a1125ec1f455ba7e9b587fcd17b
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-05-01 11:34:14 -0300

    :hammer: feat: Remove hello function import of serverless.ts and remove tsconfig.paths.ts of tsconfig.json

--- serverless.ts
+++ serverless.ts
@@ -1,35 +1,33 @@
-import type { AWS } from '@serverless/typescript';
-
-import hello from '@functions/hello';
+import type { AWS } from "@serverless/typescript";
 
 const serverlessConfiguration: AWS = {
-  service: 'certificate-ignite',
-  frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  service: "certificate-ignite",
+  frameworkVersion: "3",
+  plugins: ["serverless-esbuild"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs14.x',
+    name: "aws",
+    runtime: "nodejs14.x",
     apiGateway: {
       minimumCompressionSize: 1024,
       shouldStartNameWithService: true,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
-      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
   },
   // import the function via paths
-  functions: { hello },
+  functions: {},
   package: { individually: true },
   custom: {
     esbuild: {
       bundle: true,
       minify: false,
       sourcemap: true,
-      exclude: ['aws-sdk'],
-      target: 'node14',
-      define: { 'require.resolve': undefined },
-      platform: 'node',
+      exclude: ["aws-sdk"],
+      target: "node14",
+      define: { "require.resolve": undefined },
+      platform: "node",
       concurrency: 10,
     },
   },
