eb881e6a4fba077b70ba9fc97dc968ce63551f7a
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-05-01 19:22:18 -0300

    :hammer: feat: Create config to serverless.ts file and handler function with rule to write certificate data

--- serverless.ts
+++ serverless.ts
@@ -3,7 +3,11 @@ import type { AWS } from "@serverless/typescript";
 const serverlessConfiguration: AWS = {
   service: "certificate-ignite",
   frameworkVersion: "3",
-  plugins: ["serverless-esbuild", "serverless-offline"],
+  plugins: [
+    "serverless-esbuild",
+    "serverless-dynamodb-local",
+    "serverless-offline",
+  ],
   provider: {
     name: "aws",
     runtime: "nodejs14.x",
@@ -18,13 +22,13 @@ const serverlessConfiguration: AWS = {
   },
   // import the function via paths
   functions: {
-    hello: {
-      handler: "src/functions/hello.handler",
+    generateCertificate: {
+      handler: "src/functions/generateCertificate.handler",
       events: [
         {
           http: {
-            path: "hello",
-            method: "get",
+            path: "generateCertificate",
+            method: "post",
 
             cors: true,
           },
@@ -43,6 +47,14 @@ const serverlessConfiguration: AWS = {
       define: { "require.resolve": undefined },
       platform: "node",
       concurrency: 10,
+    },
+    dynamodb: {
+      stages: ["dev", "local"],
+      start: {
+        port: 8000,
+        inMemory: true,
+        migrate: true,
+      },
     },
   },
   resources: {
---
+++ src/functions/generateCertificate.ts
@@ -0,0 +1,40 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+
+import { document } from "../utils/dynamodb-client";
+
+interface ICreateCertificate {
+  id: string;
+  name: string;
+  grade: string;
+}
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
+
+  document
+    .put({
+      TableName: "users_certificate",
+      Item: {
+        id,
+        name,
+        grade,
+        created_at: new Date().getTime(),
+      },
+    })
+    .promise();
+
+  const response = await document
+    .query({
+      TableName: "users_certificate",
+      KeyConditionExpression: "id = :id",
+      ExpressionAttributeValues: {
+        ":id": id,
+      },
+    })
+    .promise();
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify(response.Items[0]),
+  };
+};
