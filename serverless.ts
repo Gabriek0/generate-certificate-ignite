3403fceb0090f6282e8b74f9518cd5aab5617da0
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-05-01 21:29:31 -0300

    :hammer: feat: Configure serverless.ts to deploy in aws

--- serverless.ts
+++ serverless.ts
@@ -10,6 +10,7 @@ const serverlessConfiguration: AWS = {
   ],
   provider: {
     name: "aws",
+    region: "us-east-1",
     runtime: "nodejs14.x",
     apiGateway: {
       minimumCompressionSize: 1024,
@@ -19,6 +20,14 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
+    lambdaHashingVersion: "20201221",
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: ["dynamodb:*"],
+        Resource: ["*"],
+      },
+    ],
   },
   // import the function via paths
   functions: {
