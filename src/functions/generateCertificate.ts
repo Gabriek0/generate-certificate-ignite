71487458a0256d636bdfc37ac75da06048a80eaf
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-05-03 13:39:22 -0300

    :hammer: feat: Add code to generate PDFwith puppeeter and chrome-aws-lambda

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -2,10 +2,12 @@ import { APIGatewayProxyHandler } from "aws-lambda";
 
 import { document } from "../utils/dynamodb-client";
 
-import * as dayjs from "dayjs";
+import dayjs from "dayjs";
 import { join } from "path";
 import { readFileSync } from "fs";
 import { compile } from "handlebars";
+
+import chromium from "chrome-aws-lambda";
 
 interface ICreateCertificate {
   id: string;
@@ -67,7 +69,25 @@ export const handler: APIGatewayProxyHandler = async (event) => {
 
   const content = await compileTemplate(data);
 
-  console.log(content);
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    headless: chromium.headless,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath,
+  });
+
+  const page = await browser.newPage();
+
+  await page.setContent(content);
+  await page.pdf({
+    format: "a4",
+    landscape: true,
+    printBackground: true,
+    preferCSSPageSize: true,
+    path: process.env.IS_OFFILE ? "./certificate.pdf" : null,
+  });
+
+  await browser.close();
 
   return {
     statusCode: 201,
