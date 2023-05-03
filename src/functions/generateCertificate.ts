bfc890e68b4dd585226c2d838ab4efb15a8b23b1
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-05-02 22:11:31 -0300

    :hammer: feat: Add code to generate certificate hbs content

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -2,11 +2,32 @@ import { APIGatewayProxyHandler } from "aws-lambda";
 
 import { document } from "../utils/dynamodb-client";
 
+import * as dayjs from "dayjs";
+import { join } from "path";
+import { readFileSync } from "fs";
+import { compile } from "handlebars";
+
 interface ICreateCertificate {
   id: string;
   name: string;
   grade: string;
 }
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
+
+const compileTemplate = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), "src", "templates", "certificate.hbs");
+
+  const html = readFileSync(filePath, "utf-8");
+
+  return compile(html)(data);
+};
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
@@ -32,6 +53,21 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       },
     })
     .promise();
+
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+  const medal = readFileSync(medalPath, "base64");
+
+  const data: ITemplate = {
+    id,
+    name,
+    grade,
+    date: dayjs().format("DD/MM/YYYY"),
+    medal: medal,
+  };
+
+  const content = await compileTemplate(data);
+
+  console.log(content);
 
   return {
     statusCode: 201,
