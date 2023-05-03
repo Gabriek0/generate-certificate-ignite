a259525d7c47875efb271def9932528d464bba41
Gabriel Henrique <gabriel.sanches170@gmail.com>
2023-05-03 15:28:48 -0300

    :hammer: feat: Create verifyCertificate function

--- serverless.ts
+++ serverless.ts
@@ -49,6 +49,19 @@ const serverlessConfiguration: AWS = {
         },
       ],
     },
+    verifyCertificate: {
+      handler: "src/functions/verifyCertificate.handler",
+      events: [
+        {
+          http: {
+            path: "verifyCertificate/${id}",
+            method: "get",
+
+            cors: true,
+          },
+        },
+      ],
+    },
   },
   package: { individually: true },
   custom: {
---
+++ src/functions/verifyCertificate.ts
@@ -0,0 +1,43 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { document } from "../utils/dynamodb-client";
+
+interface IUserCertificate {
+  id: string;
+  name: string;
+  grade: string;
+  created_at: string;
+}
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const { id } = event.pathParameters;
+
+  const response = await document
+    .query({
+      TableName: "users_certificate",
+      KeyConditionExpression: "id = :id",
+      ExpressionAttributeValues: {
+        ":id": id,
+      },
+    })
+    .promise();
+
+  const userCertificate = response.Items[0] as IUserCertificate;
+
+  if (userCertificate) {
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        message: "Certificado válido.",
+        name: userCertificate.name,
+        url: `https://certificate-generator-student-ignite.s3.amazonaws.com/${id}.pdf`,
+      }),
+    };
+  }
+
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      message: "Certificado inválido!",
+    }),
+  };
+};
